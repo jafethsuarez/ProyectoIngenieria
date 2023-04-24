d2e102bee575de4c90f8356cf20c591e4d93bd42
Daniel <hevcuesta@gmail.com>
2023-04-24 09:32:13 +0200

    Vista Restaurante

--- ProyectoIngenieria/src/Controller/ControllerPrincipalRestaurante.java
+++ ProyectoIngenieria/src/Controller/ControllerPrincipalRestaurante.java
@@ -2,17 +2,36 @@ package Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseEvent;
 
 public class ControllerPrincipalRestaurante {
 
     @FXML
-    private ResourceBundle resources;
+    private Label PanelControl;
 
     @FXML
-    private URL location;
+    private TableColumn<?, ?> ColumnUsuario;
 
     @FXML
-    void initialize() {
+    private TableColumn<?, ?> ColumnTelefo;
+
+    @FXML
+    private TableColumn<?, ?> ColumnHora;
+
+    @FXML
+    private TableColumn<?, ?> ColumnMesa;
+
+    @FXML
+    void menu(MouseEvent event) {
 
     }
+
+    @FXML
+    void salir(MouseEvent event) {
+
+    }
+
 }
+
